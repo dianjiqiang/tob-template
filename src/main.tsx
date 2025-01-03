fe85d91f4f9e8f37553eccfd7f98e294e8d79cbb
dianjiqiang <609661157@qq.com>
2025-01-04 00:13:05 +0800

    feat: 持续集成

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import "assets/css/reset.css"
 import "assets/css/common.css"
 import ThemeProvider from "context/ThemeContext";
 import { HashRouter } from 'react-router-dom'
-import App from './App.tsx'
+import { Provider } from 'react-redux'
+import store from '@/store'
+import App from '@/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
       <ThemeProvider>
         <HashRouter>
           <App />
         </HashRouter>
       </ThemeProvider>
+    </Provider>
   </StrictMode>,
 )
